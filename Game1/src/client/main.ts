4eb771dc965e10cc15f726d5615e2ba0d116c5bd
Sejin-cpp <sejin.groll@icloud.com>
2022-02-08 08:56:03 +0100

    first commit

---
+++ Game1/src/client/main.ts
@@ -0,0 +1,18 @@
+import Phaser from 'phaser'
+import 'regenerator-runtime/runtime'
+import HelloWorldScene from './scenes/HelloWorldScene'
+
+const config: Phaser.Types.Core.GameConfig = {
+	type: Phaser.AUTO,
+	width: 600,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 }
+		}
+	},
+	scene: [HelloWorldScene]
+}
+
+export default new Phaser.Game(config)
