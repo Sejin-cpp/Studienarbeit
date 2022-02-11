df11ac6a866643a45a719ddcd1860ad1fe5c9b5a
Sejin-cpp <sejin.groll@icloud.com>
2022-02-11 17:07:20 +0100

    Neue Schema

--- Game1/src/server/rooms/schema/GaigelState.ts
+++ Game1/src/server/rooms/schema/GaigelState.ts
@@ -1,7 +1,50 @@
-import { Schema, Context, type } from "@colyseus/schema";
+import { Schema, type, ArraySchema } from '@colyseus/schema'
 
-export class GaigelState extends Schema {
+class CardState extends Schema
+{
+  @type('string')
+  farbe : string
 
-  @type("string") mySynchronizedProperty: string = "Hello world";
+  @type('string')
+  art : string
+
+  constructor(farbe: string, art: string)
+  {
+    super()
+    this.farbe = farbe
+    this.art = art
+  }
+}
+
+class PlayerState extends Schema 
+{
+  @type('string')
+  id : string  
+  
+  @type([CardState])
+  cardsInHand: CardState[]
+
+  @type([CardState])
+  cardsInStich: CardState[]
+  constructor(id : string)
+  {
+    super()
+    this.id = id
+    this.cardsInHand = new ArraySchema<CardState>()
+    this.cardsInStich = new ArraySchema<CardState>()
+  }
+}
+
+export class GaigelState extends Schema 
+{
+
+  @type([PlayerState])
+  playerstates: PlayerState[]
+  constructor()
+  {
+    super()
+
+    this.playerstates = new ArraySchema<PlayerState>()
+  }
 
 }
