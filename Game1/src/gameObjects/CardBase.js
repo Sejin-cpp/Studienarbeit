bb97e57f499fa98fe0b8da84401173a90d3ce6fe
Sejin-cpp <sejin.groll@icloud.com>
2022-02-13 11:24:06 +0100

    teast

--- Game1/src/gameObjects/CardBase.js
+++ Game1/src/gameObjects/CardBase.js
@@ -1,20 +1,34 @@
 import Phaser from 'phaser'
 export default class CardBase extends Phaser.GameObjects.Container{
-    isHidden = true;
+    isHidden = false;
     constructor(data){
         let {scene,x,y,cardname,depth,color,symbol,value,cardback} = data
         let frontImg = new Phaser.GameObjects.Image(scene,0,0,cardname)
         let cardbackImg = new Phaser.GameObjects.Image(scene,0,0,cardback)
-        super(scene,x,y,[frontImg],[cardbackImg])
+        super(scene,x,y,[cardbackImg])
         this.frontImg = frontImg
         this.cardbackImg = cardbackImg
-        this.image = cardbackImg
+        this.image = frontImg
         this.depth = depth
         this.scene = scene
         this.color = color
         this.symbol = symbol
         this.value = value
         this.scene.add.existing(this);
+
+        this.scene.input.on('pointerdown', function (pointer) {
+
+            if (pointer.rightButtonDown())
+            {
+                if(isHidden){
+                    this.image = this.cardbackImg
+                }
+                else{
+                    this.image = this.frontImg
+                }
+            }
+    
+        }, this);
     }
 
     changeSide(){
