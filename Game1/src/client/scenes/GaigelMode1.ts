4fc9b78d223a9e68118d58b482cb7ff254296032
Sejin-cpp <sejin.groll@icloud.com>
2022-02-13 09:30:56 +0100

    creatingCards

--- Game1/src/client/scenes/GaigelMode1.ts
+++ Game1/src/client/scenes/GaigelMode1.ts
@@ -20,10 +20,34 @@ export default class GaigelMode1 extends Phaser.Scene
 	preload()
     {
         this.load.image('eichel7','assets/eichel7.png')
-        this.load.image('eichelAss','assets/eichel7.png')
-        this.load.image('eichelKoenig','assets/eichel7.png')
-        this.load.image('eichelOber','assets/eichel7.png')
-        this.load.image('eichelUnter','assets/eichel7.png')
+        this.load.image('eichel10','assets/eichel10.png')
+        this.load.image('eichelAss','assets/eichelAss.png')
+        this.load.image('eichelKoenig','assets/eichelKoenig.png')
+        this.load.image('eichelOber','assets/eichelOber.png')
+        this.load.image('eichelUnter','assets/eichelUnter.png')
+        
+        this.load.image('blatt7','assets/blatt7.png')
+        this.load.image('blatt10','assets/blatt10.png')
+        this.load.image('blattAss','assets/blattAss.png')
+        this.load.image('blattKoenig','assets/blattKoenig.png')
+        this.load.image('blattOber','assets/blattOber.png')
+        this.load.image('blattUnter','assets/blattUnter.png')
+
+        this.load.image('herz7','assets/herz7.png')
+        this.load.image('herz10','assets/herz10.png')
+        this.load.image('herzAss','assets/herzAss.png')
+        this.load.image('herzKoenig','assets/herzKoenig.png')
+        this.load.image('herzOber','assets/herzOber.png')
+        this.load.image('herzUnter','assets/herzUnter.png')
+
+        this.load.image('schellen7','assets/schellen7.png')
+        this.load.image('schellen10','assets/schellen10.png')
+        this.load.image('schellenAss','assets/schellenAss.png')
+        this.load.image('schellenKoenig','assets/schellenKoenig.png')
+        this.load.image('schellenOber','assets/schellenOber.png')
+        this.load.image('schellenUnter','assets/schellenUnter.png')
+
+        this.load.image('cardback','assets/cardback.png')
     }
 
     async create()
@@ -32,24 +56,11 @@ export default class GaigelMode1 extends Phaser.Scene
 
        console.log(room.sessionId)
        
-       room.onStateChange(state => {
+       room.onStateChange.once(state => {
            console.dir(state)
+           this.createCardObjects();
+           room.state.setCardsInDeck(this.cards)
        })
-       this.cards.push(new CardDraggable({
-        scene: this,
-        x:200,
-        y:200,
-        cardname: 'eichel7',
-        depth: 1,
-        color : "eichel",
-        symbol : "sieben",
-        value : 0,
-        ondragend: (pointer,gameObject) => {},
-        width: 50,
-        height: 100
-       }))
-
-       room.state.setCardsInDeck(this.cards)
         
        /*
        room.onMessage('keydown',(message) => {
@@ -59,5 +70,303 @@ export default class GaigelMode1 extends Phaser.Scene
            room.send('keydown',evt.key)
        })
        */
+    }
+
+    createCardObjects(){
+        //Eichel Karten*2
+        for (var i = 0; i < 2; i++){
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'eichel7',
+                depth: 1,
+                color : "eichel",
+                symbol : "sieben",
+                value : 0,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'eichel10',
+                depth: 1,
+                color : "eichel",
+                symbol : "zehn",
+                value : 10,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'eichelUnter',
+                depth: 1,
+                color : "eichel",
+                symbol : "unter",
+                value : 2,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'eichelOber',
+                depth: 1,
+                color : "eichel",
+                symbol : "ober",
+                value : 3,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'eichelKoenig',
+                depth: 1,
+                color : "eichel",
+                symbol : "koenig",
+                value : 4,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+        }
+
+        //Blatt Karten*2
+        for (var i = 0; i < 2; i++){
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'blatt7',
+                depth: 1,
+                color : "blatt",
+                symbol : "sieben",
+                value : 0,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'blatt10',
+                depth: 1,
+                color : "blatt",
+                symbol : "zehn",
+                value : 10,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'blattUnter',
+                depth: 1,
+                color : "blatt",
+                symbol : "unter",
+                value : 2,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'blattOber',
+                depth: 1,
+                color : "blatt",
+                symbol : "ober",
+                value : 3,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'blattKoenig',
+                depth: 1,
+                color : "blatt",
+                symbol : "koenig",
+                value : 4,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+        }
+
+        //Herz Karten*2
+        for (var i = 0; i < 2; i++){
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'herz7',
+                depth: 1,
+                color : "herz",
+                symbol : "sieben",
+                value : 0,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'herz10',
+                depth: 1,
+                color : "herz",
+                symbol : "zehn",
+                value : 10,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'herzUnter',
+                depth: 1,
+                color : "herz",
+                symbol : "unter",
+                value : 2,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'herzOber',
+                depth: 1,
+                color : "herz",
+                symbol : "ober",
+                value : 3,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'herzKoenig',
+                depth: 1,
+                color : "herz",
+                symbol : "koenig",
+                value : 4,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+        }
+
+        //Schellen Karten*2
+        for (var i = 0; i < 2; i++){
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'schellen7',
+                depth: 1,
+                color : "schellen",
+                symbol : "sieben",
+                value : 0,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'schellen10',
+                depth: 1,
+                color : "schellen",
+                symbol : "zehn",
+                value : 10,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'schellenUnter',
+                depth: 1,
+                color : "schellen",
+                symbol : "unter",
+                value : 2,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'schellenOber',
+                depth: 1,
+                color : "schellen",
+                symbol : "ober",
+                value : 3,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+            this.cards.push(new CardDraggable({
+                scene: this,
+                x:200,
+                y:200,
+                cardname: 'schellenKoenig',
+                depth: 1,
+                color : "schellen",
+                symbol : "koenig",
+                value : 4,
+                cardback: 'cardback',
+                ondragend: (pointer,gameObject) => {},
+                //width: 50,
+                //height: 100
+            }))
+        }
     }
 }
--- Game1/src/gameObjects/CardBase.js
+++ Game1/src/gameObjects/CardBase.js
@@ -1,15 +1,28 @@
 import Phaser from 'phaser'
 export default class CardBase extends Phaser.GameObjects.Container{
+    isHidden = true;
     constructor(data){
-        let {scene,x,y,cardname,depth,color,symbol,value} = data
-        let image = new Phaser.GameObjects.Image(scene,0,0,cardname)
-        super(scene,x,y,[image])
-        this.image = image
+        let {scene,x,y,cardname,depth,color,symbol,value,cardback} = data
+        let frontImg = new Phaser.GameObjects.Image(scene,0,0,cardname)
+        let cardbackImg = new Phaser.GameObjects.Image(scene,0,0,cardback)
+        super(scene,x,y,[frontImg],[cardbackImg])
+        this.frontImg = frontImg
+        this.cardbackImg = cardbackImg
+        this.image = cardbackImg
         this.depth = depth
         this.scene = scene
         this.color = color
         this.symbol = symbol
         this.value = value
         this.scene.add.existing(this);
+    }
+
+    changeSide(){
+        if(isHidden){
+            this.image = this.cardbackImg
+        }
+        else{
+            this.image = this.frontImg
+        }
     }
 }
